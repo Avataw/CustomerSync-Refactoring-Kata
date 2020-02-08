ccfd014ea1e3a150a60414f5e06da0f6a8df587a
Gravetow <wruszczak@web.de>
2020-02-08 13:44:08 +0100

    Remove unnecessary setters

--- java/src/main/java/codingdojo/model/Address.java
+++ java/src/main/java/codingdojo/model/Address.java
@@ -15,23 +15,7 @@ public class Address {
         return street;
     }
 
-    public String getPostalCode() {
-        return postalCode;
-    }
+    public String getPostalCode() { return postalCode; }
 
-    public void setPostalCode(String postalCode) {
-        this.postalCode = postalCode;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
+    public String getCity() { return city; }
 }
